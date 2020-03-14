2d0859febe1cd826cf95a97beab35bb2618fa7bc
David Flasterstein <davidflasterstein@wustl.edu>
2020-03-14 12:27:34 -0700

    locations

--- locs/location.py
+++ locs/location.py
@@ -1,2 +1,17 @@
-import numpy as np
-x =  5
+class location:
+    def __init__(self, id):
+        '''
+        by default theyre uninfected
+        '''
+        self.people = set()
+        self.id = id
+
+    def person_enters(person):
+        self.people.add(person)
+    
+    def person_exits(person):
+        self.people.remove(person)
+
+    
+
+
