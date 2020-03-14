9a1a988e174b9a553fa4857f7cff7e78883ea1b3
Maksym Zhelyeznyakov <demroz@localhost.localdomain>
2020-03-14 14:17:54 -0700

    sim

--- sim/simulation.py
+++ sim/simulation.py
@@ -9,6 +9,8 @@ class Simulation:
         self.people = set()
         self.locations = set()
         self.numTics = numTics
+        self.numPeople = numPeople
+        
         for i in range(numPeople):
             person = Person(i)
             if random() < .1:
@@ -17,8 +19,45 @@ class Simulation:
         for i in range(numLocations)
             locations.add(Location(i))
     
-    def runSimulation()
+    def runSimulation(self):
         for tic in range(numTics):
+            for person in self.people:
+                runTic(person)
+
+    def runTic(self, person):
+        isInfected = person.isInfected()
+
+        p_move = 0. # 0% chance of the person moving
+
+        p_wash = 0.25 # 25% of washing hands
+        p_interact = 0.5 # 50% chance of interacting with a different person
+
+        wash_dum = random.uniform(0,1)
+
+        if wash_dum < p_wash:
+            person.carrier = 0
+            
+        interact_dum = random.uniform(0,1)
+        if interact_dum < p_interact:
+            carrier_flag = person.interact()
+            p_spread = 0.1 # 10% chance of spreading
+            spread_dum = random.uniform(0,1)
+
+            if spread_dum < p_spread:
+                infect_id = random.randint(0,self.numPeople)
+                
+                p_carrirer = 0.1
+                carrier_dum = random.uniform(0,1)
+                if carrier_dum < p_carrier:
+                    self.people[infect_id].carrier = 1
+                if carrier_dum >= p_carrier:
+                    self.people[infect_id].infected = 1
+        
+            
+        
+        
+        
+        
 
 
 
